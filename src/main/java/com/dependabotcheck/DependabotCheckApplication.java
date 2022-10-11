16012fbdb0765de028096f31bfe58f2f84c1901c
joshuachristian00 <joshua_christian00@hotmail.com>
2022-10-11 16:25:57 +0200

    new project

---
+++ src/main/java/com/dependabotcheck/DependabotCheckApplication.java
@@ -0,0 +1,13 @@
+package com.dependabotcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependabotCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dependabotcheck/DependabotCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dependabotcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
